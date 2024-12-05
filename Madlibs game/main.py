a5d64b1352d9930f6f2e542788776e6c32dda9a3
Besbes Mohamed Ali <115743311+vod65dok@users.noreply.github.com>
2024-12-06 00:43:23 +0100

    Add files via upload

---
+++ Madlibs game/main.py
@@ -0,0 +1,11 @@
+adjective1 = input(" enter an Adjective1: ")
+noun = input(" enter a noun(person , place , thing) : ")
+adjective2 = input(" enter an Adjective2: ")
+verb1 = input(" enter a verb ending with 'ing': ")
+adjective3 = input(" enter an Adjective3(description): ")
+
+print(f"Today i want to a {adjective1} zoo")
+print(f"In a exhibit , I saw a {noun} ")
+print(f"{noun} was {adjective2} and {verb1}")
+print(f"I was {adjective3} !")
+
